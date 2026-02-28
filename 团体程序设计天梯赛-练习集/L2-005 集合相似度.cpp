cbcbd0f025e75000858dcb6edbd05a79fcc78076
ofb777 <ofb241223@gmail.com>
2026-03-01 00:50:18 +0800

    solved L2-005

---
+++ 团体程序设计天梯赛-练习集/L2-005 集合相似度.cpp
@@ -0,0 +1,30 @@
+/*
+  @pintia psid=994805046380707840 pid=994805070149828608 compiler=GXX
+  ProblemSet: 团体程序设计天梯赛-练习集
+  Title: 集合相似度
+  https://pintia.cn/problem-sets/994805046380707840/exam/problems/type/7?problemSetProblemId=994805070149828608
+*/
+// @pintia code=start
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+const int M = 200005;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+void solve()
+{
+    
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+// @pintia code=end
